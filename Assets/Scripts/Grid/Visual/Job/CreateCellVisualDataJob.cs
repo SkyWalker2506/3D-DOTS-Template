a0963a3e6bd4d07dc3cb049161050b7e8640dc12
Musab Kara <42008533+SkyWalker2506@users.noreply.github.com>
2024-06-01 04:43:43 +0300

    Update CreateCellVisualDataJob.cs

--- Assets/Scripts/Grid/Visual/Job/CreateCellVisualDataJob.cs
+++ Assets/Scripts/Grid/Visual/Job/CreateCellVisualDataJob.cs
@@ -1,40 +1,36 @@
 using SkyWalker.DOTS.Grid.ComponentData;
 using SkyWalker.DOTS.Grid.Visual.ComponentData;
-using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
-using UnityEngine;
 
 namespace SkyWalker.DOTS.Grid.Visual.Job
 {
-    [BurstCompile]
     public partial struct CreateCellVisualDataJob : IJobEntity
     {
-        public EntityCommandBuffer.ParallelWriter ECB;
+        public EntityCommandBuffer.ParallelWriter ParalelWriter;
 
-        [BurstCompile]
         public void Execute(in Entity entity, in DynamicBuffer<GridCellBuffer> gridCellBuffer, in CreateMapVisualData ShowMapVisualData, [ChunkIndexInQuery] int index)
         {
-            Debug.Log(gridCellBuffer.Length);
+           // Debug.Log(gridCellBuffer.Length);
             foreach (var buffer in gridCellBuffer)
             {
                 var gridCell = buffer.GridCell;
-                Debug.Log(gridCell.GridPosition);
+               // Debug.Log(gridCell.GridPosition);
                // ECB.AddComponent(index, gridCell.Entity, new CellVisualData {Visual =  ShowMapVisualData.VisualPrefab});
-                var visual = ECB.Instantiate(0, ShowMapVisualData.VisualPrefab);
+                var visual = ParalelWriter.Instantiate(index, ShowMapVisualData.VisualPrefab);
                
               
                 var size = gridCell.CellSize * .9f;
-                ECB.AddComponent(0, visual, LocalTransform.Identity);
-                ECB.SetComponent(0, visual, new PostTransformMatrix {Value = float4x4.Scale(size.x, 1, size.y)});
-                ECB.AddComponent(0,gridCell.Entity, new CellVisualData {Visual = visual});
-                ECB.AddComponent(0, visual, new Parent {Value = gridCell.Entity});
-                Debug.Log(gridCell.Entity);
-
+                //ParalelWriter.AddComponent(index, visual, LocalTransform.FromPosition(new float3(gridCell.WorldPosition.x, 0, gridCell.WorldPosition.y)));
+                ParalelWriter.SetComponent(index, visual, new PostTransformMatrix {Value = float4x4.Scale(size.x, 1, size.y)});
+                ParalelWriter.SetComponent(index, gridCell.Entity, new CellVisualData {Visual = visual});
+                //ParalelWriter.AddComponent(index, gridCell.Entity, new CellVisualData {Visual = visual});
+                //ParalelWriter.AddComponent(index, visual, new Parent {Value = gridCell.Entity});
+                //Debug.Log(gridCell.Entity);
 
             }
-                ECB.SetComponentEnabled<CreateMapVisualData>(0, entity, false);
+            ParalelWriter.SetComponentEnabled<CreateMapVisualData>(index, entity, false);
             
         }
     }
