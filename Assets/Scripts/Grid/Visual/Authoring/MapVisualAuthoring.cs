ba61753cff671aca2e34fcd9387cb1c861113940
Musab Kara <42008533+SkyWalker2506@users.noreply.github.com>
2024-06-01 04:43:22 +0300

    Update MapVisualAuthoring.cs

--- Assets/Scripts/Grid/Visual/Authoring/MapVisualAuthoring.cs
+++ Assets/Scripts/Grid/Visual/Authoring/MapVisualAuthoring.cs
@@ -6,12 +6,13 @@ namespace SkyWalker.DOTS.Grid.Visual.Authoring
 {
     public class MapVisualAuthoring : MonoBehaviour
     {
+        
         [SerializeField] private GameObject visualPrefab;
         class MapVisualBaker : Baker<MapVisualAuthoring>
         {
             public override void Bake(MapVisualAuthoring authoring)
             {
-                var entity = GetEntity(TransformUsageFlags.NonUniformScale);
+                var entity = GetEntity(TransformUsageFlags.None);
                 var visual = GetEntity(authoring.visualPrefab, TransformUsageFlags.Renderable|TransformUsageFlags.NonUniformScale|TransformUsageFlags.WorldSpace);
                 AddComponent(entity, new CreateMapVisualData
                 {
